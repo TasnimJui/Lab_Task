b22d8becff22c39ac37d7573803952d3a3edc25d
Tasnim Jui <124454340+TasnimJui@users.noreply.github.com>
2023-02-07 01:54:20 +0600

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,23 @@
+package com.codes.solution;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int A = 5;
+        int B = 6;
+        int C = 7;
+        int D = 8;
+        
+        
+        
+        System.out.println( + (A * B - C * D));
+         System.out.println( + (2 * A - B + 3 * D));
+         System.out.println( + (A * A + B * B - C * C + D * D));
+         System.out.println( + (A * A * A + B  - C * C));
+       
+        
+      
+       
+        
+    }
+}
